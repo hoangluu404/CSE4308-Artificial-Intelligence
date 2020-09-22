017df2d4d37900de614ea0936f808a6ef717ffc9
Hoang Luu <hoang.luu8@mavs.uta.edu>
2020-09-21 21:43:41 -0500

    a

--- assignment1_code_hxl9998/find_route.py
+++ assignment1_code_hxl9998/find_route.py
@@ -17,24 +17,20 @@ h_text
 # perform recursive Uniform Cost Search (UCS)
 # return total distance and nodes expanded and nodes generated and the cities expanded
 # return [ distance, [ nodes expanded, nodes generated ], [ cities ] ]
-def uniform_cost_search(fringe, visited, city_map, source, destination, nodes, route, path):
+def uniform_cost_search(fringe, visited, city_map, source, destination, nodes, path):
 
     # if fringe empty, return -1
-    if( not fringe ): return [-1, nodes, route]
+    if( not fringe ): return [[-1, nodes]]
 
     # compare and find minimum distance in the fringe
     if( fringe ):
         closest_city = min(fringe, key=fringe.get)
         nodes[0] = nodes[0]+1
-
-        route.append(closest_city)
         
         # if that minimum element is destination, return cost of the path
         if( closest_city == destination ):
-            print(str(source) + ' to ' + str(closest_city) + ' with distance ' + str(fringe[closest_city]))
-            print('\n\n')
-            
-            return [fringe[closest_city], nodes, route]
+            #print(str(source) + ' to ' + str(closest_city) + ' with distance ' + str(fringe[closest_city]))
+            return [fringe[closest_city], nodes]
 
         # put that element into visited to be visited
         cost_current = fringe.pop(closest_city)
@@ -58,18 +54,18 @@ def uniform_cost_search(fringe, visited, city_map, source, destination, nodes, r
                 if(fringe[city][0] > cost_current[0] + city_map[closest_city][city]):
                     fringe[city] = [cost_current[0] + city_map[closest_city][city], [cost_current[1], city]]
 
-            
-    print(fringe)
-    return uniform_cost_search(fringe, visited, city_map, source, destination, nodes, route, path)
+    return uniform_cost_search(fringe, visited, city_map, source, destination, nodes, path)
     
 
-def print_route(route):
-    if( not route): return
-    if( len(route) == 1): print(route[0])
+def extract_route(route, path):
+    if( not route): return path
+    if( len(route) == 1): 
+        path.append(route[0])
     else:
-        print(route[1])
+        path.append(route[1])
         route = route[0]
-        print_route(route)
+        extract_route(route, path)
+        return path
 
 
 def uninformed_search(city_map, source, destination):
@@ -79,38 +75,25 @@ def uninformed_search(city_map, source, destination):
     fringe.update({source: [0,[source]] })
     path = [source]
     # return the shortest distance between source and destination if available
-    result = uniform_cost_search(fringe, visited, city_map, source, destination, [0, 0], [], [source])
+    result = uniform_cost_search(fringe, visited, city_map, source, destination, [0, 0], [source])
     
- 
-    if(result[0][0]>-1):
+    if(result[0][0] > -1):
         print('nodes expanded: ' + str(result[1][0]) + '\nnodes generated: ' + str(result[1][1]) )
         print('distance: ' + str(result[0][0]) + ' km')
         print('route:')
         route = result[0][1]
-        route = print_route(route)
-        print(route)
+        route = extract_route(route, [])
+        if(route):
+            route.reverse()
+            for i in range (len(route)-1) :
+                print(route[i], 'to' , route[i+1], city_map[route[i]][route[i+1]], 'km')
+        else: print(source, 'to', str(destination) + ',', result[0][0] ,'km')
+        print('')
     else:
-        print('nodes expanded: ' + str(result[1][0]) + '\nnodes generated: ' + str(result[1][1]) )
+        print(result)
+        print('nodes expanded: ' + str(result[0][1][0]) + '\nnodes generated: ' + str(result[0][1][1]) )
         print('distance: infinity')
         print('route:\nnone')
-
-    print('')
-    # fringe = []
-
-    # fringe.append(source)
-    # new_map = {}
-    # for city in route:
-    #     new_map[city] = city_map[city]
-
-    
-    #print(new_map)
-
-    #route = []
-    #print(dfs(fringe, new_map, destination, [], 0, result[0], source))
-    #print(cost)
-
-
-
 
 def informed_search():
     print('INFORMED')
@@ -145,32 +128,37 @@ def main():
     city_map = temp
     
     # check to see whether city exists
-    try:
-        city_map[source]
-    except:
-        print(source + ': does not exist')
-        try:
-            city_map[destination]
-        except:
-            print(destination + ': does not exist')
-            return
-    try:
-        city_map[destination]
-    except:
-        print(destination + ': does not exist')
-        return
+    # try:
+    #     city_map[source]
+    # except:
+    #     print(source + ': does not exist')
+    #     try:
+    #         city_map[destination]
+    #     except:
+    #         print(destination + ': does not exist')
+    #         return
+    # try:
+    #     city_map[destination]
+    # except:
+    #     print(destination + ': does not exist')
+    #     return
     
+    if( not source in city_map ):
+        print(source, 'does not exist')
+        if( not destination in city_map ):
+            print(destination, 'does not exist')
+        return
+    elif( not destination in city_map ):
+        print(destination, 'does not exist')
+    
+
+
     if( len(sys.argv) == 4): # no h_file -> uninformed search
         uninformed_search(city_map, source, destination)
         
     elif( len(sys.argv) > 4): # if has h_file -> informed search
         informed_search()
     
-
-
-
-
-
 
 if __name__ == "__main__":
     main()
